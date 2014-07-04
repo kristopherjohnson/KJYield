076ceb757700a096c8f8f9d2fdbf4bc2422e4f13
Kristopher Johnson <kris@kristopherjohnson.net>
2014-07-04 13:45:23 -0400

    Initial Commit

---
+++ KJYield/KJYield.h
@@ -0,0 +1,19 @@
+//
+//  KJYield.h
+//  KJYield
+//
+//  Created by Kristopher Johnson on 7/4/14.
+//  Copyright (c) 2014 Kristopher Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KJYield.
+FOUNDATION_EXPORT double KJYieldVersionNumber;
+
+//! Project version string for KJYield.
+FOUNDATION_EXPORT const unsigned char KJYieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KJYield/PublicHeader.h>
+
+
